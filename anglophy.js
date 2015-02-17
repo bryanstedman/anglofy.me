fcabc3244a3c43186c3ae72d2f81fdc5f76040d6
Bryan Stedman <bryanbstedman@gmail.com>
2015-02-16 21:36:29 -0700

    Add ability to translate from UK to US

--- anglophy.js
+++ anglophy.js
@@ -36,4 +36,23 @@ window.onload = function () {
       htmlreplace(data);
     });
   }
+  if (queryString.en == "us" || queryString.en == "simplified") {
+    $.getJSON('http://anglophy.me/phrases.json', function(data) {
+      function htmlreplace(a, element) {
+        if (!element) element = document.body;
+        var nodes = element.childNodes;
+        for (var n=0; n<nodes.length; n++) {
+          if (nodes[n].nodeType == Node.TEXT_NODE) {
+            for (var key in a) {
+              var r = new RegExp('\\b'+a[key]+'\\b', 'g');
+              nodes[n].textContent = nodes[n].textContent.replace(r, key);
+            }
+          } else {
+            htmlreplace(a, nodes[n]);
+          }
+        }
+      }
+      htmlreplace(data);
+    });
+  }
 };
